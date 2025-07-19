e33c35b7ffa413392aa1a54625e77e564bb946bd
Teddy Christian <christianteddy90@gmail.com>
2025-07-19 23:47:32 +0200

    🎨 use the deltatime to improve the FPS

--- main.py
+++ main.py
@@ -107,9 +107,10 @@ class MainWidget(Widget):
         return int(tr_x), int(tr_y)
     
     def update(self, dt):
+        time_factor = dt * 60
         self.update_vertical_lines()
         self.update_horizontal_lines()
-        self.current_offset_y += self.SPEED
+        self.current_offset_y += self.SPEED * time_factor
 
         spacing_y = self.H_LINES_SPACING * self.height
         if self.current_offset_y >= spacing_y:
