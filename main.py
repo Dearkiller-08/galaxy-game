5f13f36b30b66fe72ab567d580d3e0e3ef548c9f
Teddy Christian <christianteddy90@gmail.com>
2025-07-20 02:31:22 +0200

    🎨 update the tiles animation to be contained inside the perspective

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from kivy.config import Config
 Config.set('graphics', 'width', '900')
 Config.set('graphics', 'height', '400')
@@ -17,15 +18,15 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0.0)
     perspective_point_y = NumericProperty(0.0)
 
-    V_NB_LINES = 4
-    V_LINES_SPACING = .1 # percentage in screen 
+    V_NB_LINES = 8
+    V_LINES_SPACING = .2 # percentage in screen 
     vertical_lines = []
 
     H_NB_LINES = 8
     H_LINES_SPACING = .15 # percentage in screen
     horizontal_lines = []
 
-    SPEED = 1
+    SPEED = 2
     current_offset_y = 0
     current_y_loop = 0
 
@@ -33,7 +34,7 @@ class MainWidget(Widget):
     current_speed_x = 0
     current_offset_x = 0
 
-    NB_TILES = 4
+    NB_TILES = 8
     tiles = []
     tiles_coordinates = []
 
@@ -42,6 +43,7 @@ class MainWidget(Widget):
         self.init_vertical_lines()
         self.init_horizontal_lines()
         self.init_tiles()
+        self.pre_fill_tiles_coordinates()
         self.generate_tiles_coordinates()
 
         if self.is_desktop():
@@ -60,9 +62,14 @@ class MainWidget(Widget):
             Color(1, 1, 1)
             for i in range(0, self.NB_TILES):
                 self.tiles.append(Quad())
+    
+    def pre_fill_tiles_coordinates(self):
+        for i in range(0, 20):
+            self.tiles_coordinates.append((0, i))
 
     def generate_tiles_coordinates(self):
-        last_y = self.current_y_loop
+        last_x = 0
+        last_y = 0
 
         for i in range(len(self.tiles_coordinates) - 1, -1, -1):
             if self.tiles_coordinates[i][1] < self.current_y_loop:
@@ -70,11 +77,32 @@ class MainWidget(Widget):
 
         if len(self.tiles_coordinates) > 0:
             last_coordinate = self.tiles_coordinates[-1]
+            last_x = last_coordinate[0]
             last_y = last_coordinate[1] + 1
 
         for i in range(len(self.tiles_coordinates), self.NB_TILES):
-            self.tiles_coordinates.append((0, last_y))
+            r = random.randint(0, 2)
+            start_index = -int(self.V_NB_LINES / 2) + 1
+            end_index = start_index + self.V_NB_LINES - 1
+            
+            if last_x <= start_index:
+                r = 1
+            elif last_x >= end_index:
+                r = 2
+
+            self.tiles_coordinates.append((last_x, last_y))
+            if r == 1:
+                last_x += 1
+                self.tiles_coordinates.append((last_x, last_y))
+                last_y += 1
+                self.tiles_coordinates.append((last_x, last_y))
+            elif r == 2:
+                last_x -= 1
+                self.tiles_coordinates.append((last_x, last_y))
+                last_y += 1
+                self.tiles_coordinates.append((last_x, last_y))
             last_y += 1
+
 
     def init_vertical_lines(self):
         with self.canvas:
