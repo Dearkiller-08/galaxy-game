58d852b598ae439de7f819cd03951bc64d1cba52
Teddy Christian <christianteddy90@gmail.com>
2025-07-21 19:33:22 +0200

    ✅ add sound-effect to the game

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from kivy.lang import Builder
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.properties import ObjectProperty
 from kivy.properties import StringProperty, NumericProperty
+from kivy.core.audio import SoundLoader
 
 Builder.load_file("menu.kv")
 
@@ -58,8 +59,15 @@ class MainWidget(RelativeLayout):
     menu_button_title = StringProperty("START")
     score_text = StringProperty()
 
+    sound_background = None
+    sound_button = None
+    sound_ship_movement = None
+    sound_game_over = None
+    sound_game_begin = None
+
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
+        self.init_audio()
         self.init_vertical_lines()
         self.init_horizontal_lines()
         self.init_tiles()
@@ -73,7 +81,24 @@ class MainWidget(RelativeLayout):
             self._keyboard.bind(on_key_down=self.on_keyboard_down)
             self._keyboard.bind(on_key_up=self.on_keyboard_up)
         Clock.schedule_interval(self.update, 1.0 / 60.0)
-    
+
+        self.sound_game_begin.play()
+        self.sound_background.play()
+
+    def init_audio(self):
+        self.sound_background = SoundLoader.load('audio/audio-background-sound.wav')
+        self.sound_button = SoundLoader.load('audio/audio-button-effect.wav')
+        self.sound_ship_movement = SoundLoader.load('audio/audio-ship-mouvement.wav')
+        self.sound_game_over = SoundLoader.load('audio/audio-game-over.mp3')
+        self.sound_game_begin = SoundLoader.load('audio/audio-game-intro.mp3')
+
+        self.sound_background.volume = 0.50
+        self.sound_game_over.volume = 1
+        self.sound_game_begin.volume = 1
+        self.sound_button.volume = 1
+        self.sound_ship_movement.volume = 1
+        self.sound_background.loop = True
+
     def reset_game(self):
         self.current_offset_y = 0
         self.current_y_loop = 0
@@ -278,9 +303,10 @@ class MainWidget(RelativeLayout):
             self.menu_title = "G  A  M  E     O  V  E  R"
             self.menu_button_title = "RESTART"
             self.menu_widget.opacity = 1
-            print("GAME OVER")
+            self.sound_game_over.play()
     
     def on_menu_button_pressed(self):
+        self.sound_button.play()
         self.reset_game()
         self.state_game_has_started = True
         self.menu_widget.opacity = 0
